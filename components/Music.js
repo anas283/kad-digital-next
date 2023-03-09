1e87580e19b6136f7855acff179da54595dda445
Muhammad Anas <muhammadanasbinadam@gmail.com>
2023-03-09 21:26:12 +0800

    update: firebase music url

--- components/Music.js
+++ components/Music.js
@@ -6,23 +6,23 @@ export default function Music() {
     },
     {
       title: "Shane Filan - Beautiful In White",
-      url: "https://od.lk/s/MjVfNTgxMDI5MjFf/Shane%20Filan%20-%20Beautiful%20In%20White.mp3"
+      url: "https://firebasestorage.googleapis.com/v0/b/kad-digital-73037.appspot.com/o/music%2FShane%20Filan%20-%20Beautiful%20In%20White.mp3?alt=media&token=0d30cdf3-33ce-42ca-ab7a-67e0d0e18738"
     },
     {
       title: "Calum Scott - You Are The Reason",
-      url: "https://od.lk/s/MjVfNTgxMDI3OTFf/Calum%20Scott%20-%20You%20Are%20The%20Reason.mp3"
+      url: "https://firebasestorage.googleapis.com/v0/b/kad-digital-73037.appspot.com/o/music%2FCalum%20Scott%20-%20You%20Are%20The%20Reason.mp3?alt=media&token=75901899-8062-44a4-af7e-5c7f093831d6"
     },
     {
       title: "Ippo Hafiz - Saat Bahagia",
-      url: "https://od.lk/s/MjVfNTgxMDI5MDNf/Ippo%20Hafiz%20-%20Saat%20Bahagia.mp3"
+      url: "https://firebasestorage.googleapis.com/v0/b/kad-digital-73037.appspot.com/o/music%2FIppo%20Hafiz%20-%20Saat%20Bahagia.mp3?alt=media&token=cde9cea6-6f8f-4775-af4a-462804626886"
     },
     {
       title: "Hael Husaini - Hari Ini",
-      url: "https://od.lk/s/MjVfNTgxMDI4MzNf/Hael%20Husaini%20-%20Hari%20Ini.mp3"
+      url: "https://firebasestorage.googleapis.com/v0/b/kad-digital-73037.appspot.com/o/music%2FHael%20Husaini%20-%20Hari%20Ini.mp3?alt=media&token=a6fe7a29-6669-4b9e-8dc4-9a0a23c1e997"
     },
     {
       title: "Virgoun - Bukti",
-      url: "https://od.lk/s/MjVfNTgxMDI5NzFf/Virgoun%20-%20Bukti.mp3"
+      url: "https://firebasestorage.googleapis.com/v0/b/kad-digital-73037.appspot.com/o/music%2FVirgoun%20-%20Bukti.mp3?alt=media&token=23024cee-d258-4161-bc47-0dd96bb9471c"
     }
 
     // {
